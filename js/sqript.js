b01e049bb0424ea65a154097e100f830fd8ab8a1
Hana <happy_bian_cam_cam_123@yahoo.co.jp>
2021-05-30 17:28:02 +0900

    レイアウト完成

--- js/sqript.js
+++ js/sqript.js
@@ -1,9 +1,28 @@
 
+
 $('.sp-nav-list').hide();
-$('.fa-bars').on('click',function(){
-    $(this).next().slideToggle();
+$('.fa-bars').on('click', function () {
+
+    $('.sp-nav-list').toggle();
+
 })
 
-$('.sp-nav-list').on('click',function(){
-    $('sp-nav-list').fadeOut();
-})
+$('.sp-nav-list').on('click', function () {
+    $('.sp-nav-list').fadeOut();
+});
+$('.fa-times').on('click', function () {
+    $('.sp-nav-list').fadeOut();
+});
+
+
+
+// スリック
+
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    // autoplaySpeed: 1000,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
+  });
